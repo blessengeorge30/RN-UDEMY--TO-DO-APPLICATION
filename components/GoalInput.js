c2ffdfa93ea96439b40b33b913eba21c9f10f8d6
Blessen George <blessen.george30@gmail.com>
2024-10-25 12:42:18 +0530

    UPDATED

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,49 +1,72 @@
 import { useState } from "react";
-import { View,TextInput,Button,StyleSheet } from "react-native";
+import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
 
 function GoalInput(props) {
     const [enteredGoalText, setEnteredGoalText] = useState('');
 
     function goalInputHandler(enteredText) {
         setEnteredGoalText(enteredText);
-      };
+    };
 
-      function addGoalhandler() {
+    function addGoalhandler() {
         props.onAddGoal(enteredGoalText);
         setEnteredGoalText('');
-      }
+    }
 
-    return(
-        <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput}
-          placeholder='Your course goal! '
-          value={enteredGoalText}
-          onChangeText={goalInputHandler} />
-        <Button title="Add Goal" onPress={addGoalhandler} />
-      </View>
+    return (
+      
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput}
+                    placeholder='Your course goal! '
+                    value={enteredGoalText}
+                    onChangeText={goalInputHandler} />
+                    <View style={styles.buttonContainer}>
+                    <View style={styles.Button}>
+                        <Button title="Add Goal" onPress={addGoalhandler} />  
+                    </View>
+                      <View style={styles.Button}>
+                         <Button title="Cancel" />
+                      </View>
+                     
+                    </View>
+                
+            </View>
+  
+
     )
 };
 
 
 
-const styles =StyleSheet.create({
+const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'space-between',
         paddingBottom: 24,
         borderBottomWidth: 1,
         borderBottomColor: '#ccc'
-    
-      },
-    
-      textInput: {
+
+    },
+
+    textInput: {
         borderWidth: 1.5,
         borderColor: '#ccc',
-        width: '75%',
+        width: '100%',
         marginRight: 8,
-        padding: 8,
-        elevation:5,
+        padding: 18,
+        elevation: 5,
         borderRadius: 5
-      },
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        alignSelf:'center'
+    },
+    Button:{
+        width: '40%',
+        marginHorizontal:10,
+        marginTop:20
+    }
+   
+
 })
 export default GoalInput;
